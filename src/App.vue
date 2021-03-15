70c77ac148756fcf871629119d4c7c3a7abb2a9d
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2021-03-15 08:30:03 +0100

    robot class and basic visualization

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
   <div id="app">
-    <canvas id="canvas">
+    <canvas id="canvas" width="400" height="400">
     </canvas>
   </div>
 </template>
 
 <script>
+import { Robot } from '@/robot-simulator';
 
 export default {
   name: 'App',
   components: {
   },
   mounted() {
+    this.robot = new Robot();
+    // this.robot.execute('0 0 NORTH AAAALAAAALAAAALAAAAL');
+    // this.robot.execute('7 3 NORTH RAALALA');
+    this.robot.execute('0 0 NORTH AAAAAARAAARAAALAA');
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     this.vueCanvas = ctx;
+    this.vueCanvas.beginPath();
+    this.vueCanvas.strokeStyle = 'blue';
+    this.movePath();
+  },
+  methods: {
+    movePath() {
+      const ctx = this.vueCanvas;
+      // ctx.lineTo(0, 200);
+      // ctx.lineTo(200, 200);
+      this.robot.history.forEach((move, i) => {
+        const action = i === 0 ? 'moveTo' : 'lineTo';
+        const [x, y] = move;
+        const toX = x * 20 + 200;
+        const toY = y * -20 + 200;
+        console.log(x, y);
+        ctx[action](toX, toY);
+      });
+      ctx.stroke();
+    },
   },
 };
 </script>
@@ -29,8 +53,6 @@ export default {
   margin-top: 60px;
 }
 #canvas {
-  width: 600px;
-  height: 400px;
   border: 1px solid #d3d3d3;
 }
 </style>
---
+++ src/robot-simulator.js
@@ -0,0 +1,60 @@
+class Robot {
+  constructor() {
+    this.history = [];
+  }
+
+  parseArgs(params) {
+    const argsArray = params.split(' ');
+    const [x, y, facing, movements] = argsArray;
+    Object.assign(this, { x: parseInt(x, 10), y: parseInt(y, 10), facing });
+    this.history = [[this.x, this.y]];
+    this.processMovements(movements.split(''));
+  }
+
+  rotate(dir) {
+    const directions = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
+    const directionIndex = directions.indexOf(this.facing);
+    const change = dir === 'L' ? -1 : 1;
+    let newDirectionIndex = directionIndex + change;
+    if (newDirectionIndex < 0) newDirectionIndex = directions.length - 1;
+    if (newDirectionIndex > directions.length) newDirectionIndex = 0;
+    this.facing = directions[newDirectionIndex];
+  }
+
+  processMovements(movements) {
+    movements.forEach((el) => {
+      if (el !== 'A') {
+        this.rotate(el);
+      } else {
+        this.step();
+      }
+    });
+  }
+
+  step() {
+    switch (this.facing) {
+      case 'EAST':
+        this.x += 1;
+        break;
+      case 'WEST':
+        this.x -= 1;
+        break;
+      case 'SOUTH':
+        this.y -= 1;
+        break;
+      default: this.y += 1;
+    }
+    this.history.push([this.x, this.y]);
+  }
+
+  toString() {
+    return Object.values(this).join(' ');
+  }
+
+  execute(params) {
+    this.parseArgs(params);
+    return this.toString();
+  }
+}
+
+module.exports = { Robot };
