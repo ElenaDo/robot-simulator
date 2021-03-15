142cd9eebb4afb3fe027fb2ec89d75e23d6bf799
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2021-03-15 18:10:18 +0100

    move animation

--- src/App.vue
+++ src/App.vue
@@ -10,33 +10,42 @@ import { Robot } from '@/robot-simulator';
 
 export default {
   name: 'App',
+  data: () => ({
+    scale: 20,
+  }),
   components: {
   },
   mounted() {
     this.robot = new Robot();
-    // this.robot.execute('0 0 NORTH AAAALAAAALAAAALAAAAL');
-    // this.robot.execute('7 3 NORTH RAALALA');
-    this.robot.execute('0 0 NORTH AAAAAARAAARAAALAA');
+    // this.robot.execute('0 3 NORTH RAALALLL');
+    this.robot.execute('0 0 NORTH RAAAALRRAAAALAARAARAAAAAA');
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
+    ctx.lineWidth = 2;
     this.vueCanvas = ctx;
     this.vueCanvas.translate(canvas.width / 2, canvas.height / 2);
-    this.vueCanvas.beginPath();
     this.vueCanvas.strokeStyle = 'blue';
     this.movePath();
+    // this.vueCanvas.beginPath();
   },
   methods: {
     movePath() {
       const ctx = this.vueCanvas;
-      this.robot.history.forEach((move, i) => {
-        const action = i === 0 ? 'moveTo' : 'lineTo';
-        const [x, y] = move;
-        const toX = x * 20;
-        const toY = y * -20;
-        console.log(x, y);
-        ctx[action](toX, toY);
-      });
-      ctx.stroke();
+      let prevX;
+      let prevY;
+      const move = (step) => {
+        ctx.beginPath();
+        const [x, y] = step;
+        const toX = x * this.scale;
+        const toY = y * -this.scale;
+        if (prevX) ctx.moveTo(prevX, prevY);
+        else ctx.moveTo(toX, toY);
+        ctx.lineTo(toX, toY);
+        prevX = toX;
+        prevY = toY;
+        ctx.stroke();
+      };
+      this.robot.history.forEach((step, i) => setTimeout(() => move(step, i), 100 * i));
     },
   },
 };
